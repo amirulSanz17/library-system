8ced86e054c7c7944533b35baa597865fa993a1e
amirulSanz17 <amirulsanz17@gmail.com>
2026-09-16 17:04:00 +0700

    updat command routing

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Routing untuk books, members, and categories
+// Routing untuk books
 Route::get('/books', [BookController::class, 'index'])
 ->name('buku');
-
 Route::get('/books/{id}', [BookController::class, 'show']);
 
+// Routing untuk members
 Route::get('/members', [MemberController::class, 'index'])
 ->name('member');
-
 Route::get('/members/{id}', [MemberController::class, 'show']);
 
+// Routing untuk categories
 Route::get('/categories', [CategoryController::class, 'index'])
 ->name('kategori');
 
+// Routing untuk dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])
 ->name('dashboard');
